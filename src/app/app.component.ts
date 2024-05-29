05c667f785014f390f340bc5a33c496d22fa2ef3
hieuleminhhh <hieuleminhhhhh@gmail.com>
2024-05-29 08:48:56 +0700

    routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+// src/app/app.component.ts
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
+import { HomeComponent } from './home/home.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [CommonModule, IonicModule], 
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  imports: [CommonModule, IonicModule, HomeComponent, RouterOutlet], 
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]  
 })
 export class AppComponent {
   title = 'ManageRestaurant';
