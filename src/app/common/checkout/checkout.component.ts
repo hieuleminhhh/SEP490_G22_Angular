d06660fbf97044ddc6533b25f73714e77cf9ad2b
DuongLV <ld4902@gmail.com>
2024-06-13 23:47:57 +0700

    update checkout and payment

--- src/app/common/checkout/checkout.component.ts
+++ src/app/common/checkout/checkout.component.ts
@@ -1,20 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { Dish } from '../../../models/dish.model';
-import { CommonModule } from '@angular/common';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule, Time } from '@angular/common';
+import { ActivatedRoute, Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CheckoutService } from '../../../service/checkout.service';
+import { CartService } from '../../../service/cart.service';
 
 @Component({
   selector: 'app-checkout',
-  standalone:true,
+  standalone: true,
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.css'],
-  imports: [CommonModule, RouterLink, RouterLinkActive]
+  imports: [CommonModule, RouterLink, RouterLinkActive, FormsModule,MatDialogModule]
 })
 export class CheckoutComponent implements OnInit {
-
+  selectedService: string = 'service1';
+  orderTime: string = 'Sớm nhất';
+  isEditing: boolean = false;
+  selectedTime!: Date;
   cartItems: Dish[] = [];
 
-  constructor() { }
+  date: string | undefined;
+  time: string | undefined;
+  isEarliest: boolean = false;
+
+  consigneeName: string = '';
+  guestPhone: string = '';
+  email: string = '';
+  address: string = '';
+  note: string = '';
+  people:number | undefined;
+  selectedPaymentMethod: string = 'delivery';
+
+  constructor(private cartService: CartService,private router: Router,private route: ActivatedRoute,private checkoutService: CheckoutService) { }
 
   ngOnInit() {
     // Lấy dữ liệu từ sessionStorage khi component được khởi tạo
@@ -22,6 +41,58 @@ export class CheckoutComponent implements OnInit {
     if (cartItemsString) {
       this.cartItems = JSON.parse(cartItemsString); // Chuyển đổi chuỗi JSON thành mảng đối tượng JavaScript
       console.log(this.cartItems);
+    }
+  }
+
+  toggleEdit() {
+    // Hiển thị modal khi nhấn vào nút "Thay đổi"
+    const modal = document.getElementById('updateTimeModal');
+    if (modal) {
+      modal.classList.add('show');
+      modal.style.display = 'block';
+    }
+  }
+
+  hideModal() {
+    // Đóng modal
+    const modal = document.getElementById('updateTimeModal');
+    if (modal) {
+      modal.classList.remove('show');
+      modal.style.display = 'none';
+    }
+  }
+
+  saveTime() {
+    if (this.isEarliest) {
+      this.orderTime = 'Sớm nhất';
+    } else {
+      // Xử lý khi người dùng nhập ngày và giờ
+      if (this.date && this.time) {
+        this.orderTime = 'Ngày:'+this.date +'  Giờ:'+ this.time;
+      } else {
+        console.error('Ngày hoặc giờ chưa được nhập.');
+        // Có thể thực hiện các xử lý khác tại đây, ví dụ hiển thị thông báo lỗi
+      }
+    }
+
+    console.log('Đã lưu thời gian:', this.orderTime);
+    this.hideModal(); // Đóng modal sau khi lưu thành công
+  }
+
+  formatDateTime(date: string, time: string): string {
+    return `${date}T${time}:00.000Z`;
+  }
+  onModalShown() {
+    // Xử lý khi modal được hiển thị
+  }
+
+  onModalHidden() {
+    // Xử lý khi modal bị ẩn đi
+  }
+  onEarliestChange() {
+    if (this.isEarliest) {
+      this.date = undefined;
+      this.time = undefined;
     }
   }
 
@@ -36,4 +107,58 @@ export class CheckoutComponent implements OnInit {
       return total + (price * item.quantity);
     }, 0).toFixed(2));
   }
+
+  navigateToPayment(): void {
+    let receivingTime:string='';
+    if (this.date && this.time) {
+      receivingTime = this.formatDateTime(this.date, this.time);
+    }else{
+      const currentTime = new Date();
+      currentTime.setHours(currentTime.getHours() + 1);
+      const currentDate = currentTime.toISOString().split('T')[0];
+      const currentTimeStr = currentTime.toTimeString().split(' ')[0].substring(0, 5);
+      receivingTime = this.formatDateTime(currentDate, currentTimeStr);
+    }
+    const request = {
+      guestPhone: this.guestPhone,
+      email: this.email,
+      addressId: 0,
+      guestAddress: this.address,
+      consigneeName: this.consigneeName,
+      orderDate: new Date().toISOString(),
+      status: 0,
+      recevingOrder: receivingTime,
+      totalAmount: 0,
+      deposits: 0,
+      cartItems: this.cartItems.map(item => ({
+        unitPrice: this.getTotalPrice(item),
+        quantity: item.quantity,
+        note: this.note,
+        dishId: item.dishId,
+        orderId: 0,
+        dishesServed: 0,
+        comboId: item.comboId
+      }))
+    };
+    console.log(request);
+    this.checkoutService.submitOrder(request).subscribe({
+      next: response => {
+        console.log('Order submitted successfully', response);
+        this.cartService.clearCart();
+        sessionStorage.removeItem('cartItems');
+        if (this.selectedPaymentMethod === 'banking') {
+          // Chuyển hướng đến trang quét mã nếu chọn "Paying through bank"
+          this.router.navigate(['/payment-scan'], { queryParams: { guestPhone: this.guestPhone } });
+        } else {
+          // Chuyển hướng đến trang thanh toán thông thường
+          this.router.navigate(['/payment'], { queryParams: { guestPhone: this.guestPhone } });
+        }
+      },
+      error: error => {
+        console.error('Error submitting order', error);
+      }
+    });
+  }
+
+
 }
