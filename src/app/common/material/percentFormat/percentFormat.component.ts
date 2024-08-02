135250b4a1747387a67b0d284a51a25616d76e47
hieuleminhhh <hieuleminhhhhh@gmail.com>
2024-08-03 03:20:30 +0700

    update discount for invoice

---
+++ src/app/common/material/percentFormat/percentFormat.component.ts
@@ -0,0 +1,11 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'percentage',
+  standalone: true
+})
+export class PercentagePipe implements PipeTransform {
+  transform(value: number): string {
+    return value + '%';
+  }
+}
