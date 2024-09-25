c253f144d4be23bcb6d812084c7e072284b5c99a
DuongLV <ld4902@gmail.com>
2024-09-25 22:22:45 +0700

    update websocket

--- src/app/staff/ManagerOrder/CreateTakeAwayOrder/CreateTakeAwayOrder.component.ts
+++ src/app/staff/ManagerOrder/CreateTakeAwayOrder/CreateTakeAwayOrder.component.ts
@@ -163,7 +163,7 @@ export class CreateTakeAwayOrderComponent implements OnInit {
     };
   }
   initializeWebSocket() {
-    this.socket = new WebSocket('ws://yourserver.com');
+    this.socket = new WebSocket('wss://localhost:7188/ws');
     this.socket.onopen = () => { /* xử lý onopen */ };
     this.socket.onmessage = (event) => { /* xử lý onmessage */ };
     this.socket.onclose = () => { /* xử lý onclose */ };
